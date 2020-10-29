0cb0d9289a355e840a68b1af9ef6c03903318044
Abigail Glunn <aglunn@gmail.com>
2020-10-28 23:54:21 -0700

    Add styling such that abort doesn't move

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let changeColor = document.getElementById('changeColor');
+let abortButton = document.getElementById('qunit-abort-tests-button');
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -7,6 +8,10 @@ chrome.storage.sync.get('color', function(data) {
 
 changeColor.onclick = function(element) {
   let color = element.target.value;
+  console.log("longer click check what");
+
+  //console.log(abortButton);
+
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
