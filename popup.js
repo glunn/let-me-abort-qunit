53f603290f2a1dc8da865a93ac857368ecfc5296
Abigail Glunn <aglunn@gmail.com>
2020-10-29 00:25:29 -0700

    Update the popup and styles

--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
 let changeColor = document.getElementById('changeColor');
-let abortButton = document.getElementById('qunit-abort-tests-button');
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -10,11 +9,25 @@ changeColor.onclick = function(element) {
   let color = element.target.value;
   console.log("longer click check what");
 
-  //console.log(abortButton);
+   /* A function creator for callbacks */
+  function doStuffWithDOM(element) {
+    alert("I received the following DOM content:\n" + element);
+    var test = document.getElementById("testhere");
+    test.innerHTML = element;
+  }
+
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { text: "report_back" }, doStuffWithDOM);
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
   });
 };
+
+const background = chrome.extension.getBackgroundPage();
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Do something, e.g. send a message to content or background script
+  background.console.log('test background');
+});
